bc2d46c46ed0e981fdbd557cfd23f038ef935f8c
Mihail Taivan <mihail.taivan.dev@gmail.com>
2026-02-21 20:48:40 +0200

    Problema Lumini: 49 puncte pbinfo

--- OMI2026/OMI2026.cpp
+++ OMI2026/OMI2026.cpp
@@ -1,20 +1,30 @@
-// OMI2026.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+#include <fstream>
+using namespace std;
+ifstream fin("lumini.in");
+ofstream fout("lumini.out");
+int val, dr, st, n, m, c, maxi, i, j, lumini[200001];
+long long suma;
 int main()
 {
-    std::cout << "Hello World!\n";
+    fin >> c >> n >> m;
+    for (i = 1; i <= m; ++i) {
+        fin >> st >> dr >> val;
+        for (j = st; j <= dr; ++j) {
+            lumini[j] += val;
+        }
+    }
+    for (i = 1; i <= n; ++i) {
+        suma += lumini[i];
+        if (lumini[i] > maxi) {
+            maxi = lumini[i];
+        }
+    }
+    if (c == 1) {
+        fout << suma;
+    }
+    else {
+        fout << maxi;
+    }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
